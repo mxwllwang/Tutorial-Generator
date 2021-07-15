bdb20d566998944e5e406b67c1fc81db31b1de88
mxwllw <mxwllw@cs.washington.edu>
2021-07-14 17:39:16 -0700

    Allow basic fetch from sqlite database functionality

--- tutorial-generator/flaskr/main.py
+++ tutorial-generator/flaskr/main.py
@@ -9,31 +9,30 @@ from flaskr.db import get_db
 
 bp = Blueprint('main', __name__, url_prefix='/main') # Declares blueprint
 
-@bp.route('/get', methods=('GET', 'POST'))
+@bp.route('/base', methods=('GET', 'POST'))
 def get():
     print('Hello World!', file=sys.stderr)
     if request.method == 'GET':
-        print("GET Request", file=sys.stderr)
-        error_message = "Jeff"
-        # error_message = request.form['error'] # get from form (total message)
-        print(error_message, file=sys.stderr)
+        error_message = "Temporary Error Message"
+        error_message = request.args['error'] # get from form (total message)
+        print("GET REQUEST: {error}".format(error = error_message))
 
         # parse error message to a recognizable format
-        flash("JEFF")
         db = get_db()
-        tutorial = db.execute(
-            "SELECT * FROM tg WHERE error = '%s'" % error_message
-        ).fetchone()
+        selected_row = db.execute('SELECT * FROM tg WHERE error=?', (error_message,)).fetchone()
 
+        tutorial = "Null Tutorial"
         error = None # default value of error        
-        if tutorial is None:
-            error = 'Error not recognized'
+        if selected_row is None:
+            error = 'Input not recognized'
+        else:
+            tutorial = selected_row['tutorial']
+            print(tutorial)
 
         if error is None:
             flash(tutorial, category='warning')
         else:
             flash(error, category='error')
-        return redirect('base.html')
-    else:
-        return render_template('base.html')
+        
+    return render_template('base.html')
             
--- tutorial-generator/flaskr/schema.sql
+++ tutorial-generator/flaskr/schema.sql
@@ -1,31 +1,15 @@
 DROP TABLE IF EXISTS tg;
 
 CREATE TABLE tg (
-  id INT, error TEXT NOT NULL, tutorial TEXT,
-  PRIMARY KEY (id)
+  id INT PRIMARY KEY, 
+  error TEXT NOT NULL,
+  tutorial TEXT
 );
 
 INSERT INTO tg (id, error, tutorial)
-VALUES (1, 'My Error', 'My Tutorial')
+VALUES (1, 'My Error', 'My Tutorial');
 
 INSERT INTO tg (id, error, tutorial)
-VALUES (2, 'My Error 2', 'My Tutorial 2')
+VALUES (2, 'My Error 2', 'My Tutorial 2');
 
 /* Don't do bulk insert for now. */
-
-
-/*
-DROP TABLE IF EXISTS err;
-DROP TABLE IF EXISTS tg;
-
-CREATE TABLE err (
-  id INT, msg TEXT NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE tg (
-  id INT, tutorial TEXT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (id) REFERENCES user (id)
-);
-*/
