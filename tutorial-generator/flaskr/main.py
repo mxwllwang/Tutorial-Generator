dc63a1f73de204283eb4ec81ae07e49a331b5b68
mxwllw <mxwllw@cs.washington.edu>
2021-07-16 01:03:28 -0700

    Enable file uploads

--- tutorial-generator/flaskr/main.py
+++ tutorial-generator/flaskr/main.py
@@ -1,11 +1,13 @@
-import functools, sys
-
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
+import functools, sys, os
+from flask import Flask, Blueprint, flash, g, redirect, render_template, request, session, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+from flaskr.db import get_db
 # from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db
+UPLOAD_FOLDER = 'flaskr/uploads'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 bp = Blueprint('main', __name__, url_prefix='/main') # Declares blueprint
 
@@ -14,7 +16,7 @@ def id():
     if request.method == 'POST':
         error_message = "Temporary Error Message"
         error_message = request.form['error'] # get from form (total message)
-        print("POST REQUEST: {error}".format(error = error_message))
+        print("POST REQUEST /id: Error is {error}".format(error = error_message))
 
         # parse error message to a recognizable format
         db = get_db()
@@ -35,29 +37,69 @@ def id():
         
     return render_template('id.html')
 
+def allowed_file(filename, extension):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() == extension
+
+# Downloadable files after upload??
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+app.add_url_rule(
+    "/uploads/<name>", endpoint="download_file", build_only=True
+)
+
 @bp.route('/file', methods=('GET', 'POST'))
 def file():
     if request.method == 'POST':
-        error_message = "Temporary Error Message"
-        error_message = request.form['error'] # get from form (total message)
-        print("POST REQUEST: {error}".format(error = error_message))
 
-        # parse error message to a recognizable format
-        db = get_db()
-        selected_row = db.execute('SELECT * FROM tg WHERE error=?', (error_message,)).fetchone()
-
-        tutorial = "Default Tutorial"
-        error = None # default value of error        
-        if selected_row is None:
-            error = 'Input not recognized'
+        # parse file extension type
+        allowed_extension = ''
+        selected_language = request.form['lang']
+        if selected_language == 'java':
+            allowed_extension = 'java'
+        elif selected_language == 'python':
+            allowed_extension = 'py'
+        elif selected_language == 'c++':
+            allowed_extension = 'cpp'
+        elif selected_language == 'text':
+            allowed_extension = 'txt'
         else:
-            tutorial = selected_row['tutorial']
-            print(tutorial)
+            flash("Language selection is unavailable")
+        
+        print("POST REQUEST /file: Allowed extension is {lang}".format(lang = allowed_extension))
 
-        if error is None:
-            flash(tutorial, category='warning')
+        # upload file
+        # check if the post request has the file part (is this necessary?)
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if the user doesn't select a file, submit empty file without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename, allowed_extension):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print("Saved file successfully")
+            # Downloads doesn't work yet
+            # return redirect(url_for('download_file', name=filename))
         else:
-            flash(error, category='error')
+            flash('Incorrect file extension')
+            return redirect(request.url)
+
+        #db = get_db()
+        #selected_row = db.execute('SELECT * FROM tg WHERE error=?', (error_message,)).fetchone()
+
+        #tutorial = "Default Tutorial"
+        #error = None # default value of error        
+        #if selected_row is None:
+        #    error = 'Input not recognized'
+        #else:
+        #    tutorial = selected_row['tutorial']
+        #    print(tutorial)
         
     return render_template('file.html')
             
